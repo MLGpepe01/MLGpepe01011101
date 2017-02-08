b5bc00c18eb31227e773172cda0be9505bf3b00b
David Sliva <sliva@citypolyhs.org>
2017-02-08 21:23:10 +0000

    asd

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // #login-box password field
+    $('#pass').attr('type', 'text');
+    $('#pass').val('Password');
+    $('button').click (function() {
+        if  (val password='123456'){
+        $("results").append("1234")});
+        
+});
